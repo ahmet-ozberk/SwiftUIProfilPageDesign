07ff72f43634f4bda1a81bdc97c9890943ca7acc
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2022-04-09 20:38:31 +0300

    Initial Commit

---
+++ SocialMediaProfile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialMediaProfile
+//
+//  Created by Ahmet OZBERK on 9.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialMediaProfile/SocialMediaProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaProfileApp.swift
+//  SocialMediaProfile
+//
+//  Created by Ahmet OZBERK on 9.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
